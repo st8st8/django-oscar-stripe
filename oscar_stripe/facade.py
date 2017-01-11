49784ecf5a76547f00688a9708eb871f82d4debf
karthikarul20 <karthikarul20@gmail.com>
2017-01-11 19:55:56 +0800

    Fix for SyntaxError: Non-ASCII character '\xc3'

--- oscar_stripe/facade.py
+++ oscar_stripe/facade.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.conf import settings
 from oscar.apps.payment.exceptions import UnableToTakePayment, InvalidGatewayRequestError
 from django.utils import timezone
